32d6ab98cd0b37af31a7e2bfacfa76277a739da8
amityadav06 <amityadav06071998@gmail.com>
2021-02-12 23:14:54 +0530

    digital clock code is updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,40 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+        time: new Date()
+    };
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+componentDidMount(){
+  console.log("Mounted is successfull")
+    this.timerID = setInterval(() => {
+        this.tick();
+    }, 1000);
 }
 
+componentWillUnmount(){
+  console.log("Component is Unmounted successfully")
+    clearInterval(this.timerID);
+}
+
+tick(){
+    this.setState({
+        time: new Date(),
+    });
+}
+
+
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
